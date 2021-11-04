cfdd6922431edf7a5b4a6fb1afd1ebf17f67c97f
oshadaera68 <mindartlk010@gmail.com>
2021-11-04 22:12:51 +0530

    updated methods from enter to student form and back to dashboard form

--- src/controller/DashboardFormController.java
+++ src/controller/DashboardFormController.java
@@ -2,12 +2,24 @@ package controller;
 
 import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import java.io.IOException;
+import java.net.URL;
 
 public class DashboardFormController {
 
     public JFXButton btnStudent;
+    public AnchorPane rootContext;
 
-    public void studentOnAction(ActionEvent actionEvent) {
-
+    public void studentOnAction(ActionEvent actionEvent) throws IOException {
+        URL resource = getClass().getResource("../views/StudentForm.fxml");
+        Parent load = FXMLLoader.load(resource);
+        Stage window = (Stage) rootContext.getScene().getWindow();
+        window.setScene(new Scene(load));
+        window.setTitle("New Student Form | StudentApp v0.1.0");
     }
 }
--- src/controller/StudentFormController.java
+++ src/controller/StudentFormController.java
@@ -1,14 +1,48 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class StudentFormController {
-    public void backOnAction(ActionEvent actionEvent) {
+    public AnchorPane studentFormContext;
+    public JFXButton btnHome;
+    public JFXButton btnNewStudent;
+    public JFXTextField txtId;
+    public JFXTextField txtName;
+    public JFXTextField txtContact;
+    public JFXTextField txtAddress;
+    public JFXButton btnSave;
+    public TableView tblStudent;
+    public TableColumn colStudentId;
+    public TableColumn colStudentName;
+    public TableColumn colContact;
+    public TableColumn colAddress;
+    public TableColumn colOperator;
+
+    public void backOnAction(ActionEvent actionEvent) throws IOException {
+        URL resource = getClass().getResource("../views/DashBoardForm.fxml");
+        Parent load = FXMLLoader.load(resource);
+        Stage window = (Stage) studentFormContext.getScene().getWindow();
+        window.setScene(new Scene(load));
+        window.setTitle("Student App v0.1.0 | MINDARTLK Creations");
     }
 
     public void newStudentOnAction(ActionEvent actionEvent) {
+
     }
 
     public void saveAndUpdateOnAction(ActionEvent actionEvent) {
+
     }
 }
