8dd06464a05162da21a91514199ec5f71aef4990
oshadaera68 <mindartlk010@gmail.com>
2021-11-04 21:57:45 +0530

    added StudentForm fxml file and controller file

---
+++ src/controller/StudentFormController.java
@@ -0,0 +1,14 @@
+package controller;
+
+import javafx.event.ActionEvent;
+
+public class StudentFormController {
+    public void backOnAction(ActionEvent actionEvent) {
+    }
+
+    public void newStudentOnAction(ActionEvent actionEvent) {
+    }
+
+    public void saveAndUpdateOnAction(ActionEvent actionEvent) {
+    }
+}
